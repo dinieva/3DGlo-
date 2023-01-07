8b572693670c542c241911f9ddaa2b011c278c8b
Dinieva Rezeda <dinievarezeda@mail.ru>
2023-01-07 03:33:33 +0300

    в инпут вводятся исключительно прописанные в регулярках символы

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -4,16 +4,9 @@ const calc = () => {
 
     calcBlockInputs.forEach(function (input) {
         input.addEventListener('input', (e) => {
-            //let isError = false;
-            if (!/[^\d]/g.test(e.target.value) && e.target.value !== '') {
-                console.log('isError = false');
-            } else {
-                //isError = true;
-                e.target.setCustomValidity('Введите число.');
-                e.target.reportValidity();
-            }
+            e.target.value = e.target.value.replace(/\D/g, '');
         });
-    })
+    });
 };
 
 export default calc
--- src/modules/validateForms.js
+++ src/modules/validateForms.js
@@ -8,27 +8,21 @@ const validate = () => {
             input.addEventListener('input', (e) => {
                 if (e.target.type === 'text') {
                     //только кириллицы в любом регистре, дефиса и пробела.
-                    const reg = /[а-яё\-\s]$/i;
-                    const text = e.target.value;
-                    console.log(reg.test(text));
+                    e.target.value = e.target.value.replace(/[^а-яё\-\s]$/i, '');
                 }
 
                 if (e.target.type === 'email') {
                     //только латиницы в любом регистре, цифры и спецсимволы:  @  -  _  . ! ~ * '
-                    const email = e.target.value;
-                    const reg = /^[\w]{1}[\w-\.-!~*']*@[\w-]+\.[a-z]{2,4}$/i;
-                    console.log(reg.test(email));
+                    e.target.value = e.target.value.replace(/^[\w]{1}[\w-\.-!~*']*@[\w-]+\.[a-z]{2,4}$/i, '');
                 }
 
                 if (e.target.type === 'tel') {
                     // ввод только цифр, круглых скобок и дефис
-                    const reg = /^[\d\+][\d\(\)\-]*\d$/;
-                    console.log(reg.test(e.target.value));
-                }
-
+                    e.target.value = e.target.value.replace(/[^\d\(\)\-]/g, '');
+                    ///^[^\d\+][\d\(\)\-]*\d$/
+                };
             });
         });
     });
-};
-
+}
 export default validate 
