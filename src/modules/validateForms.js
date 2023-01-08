7fa96e51e7c91fb1eb12e1144212cee2800f354a
Dinieva Rezeda <dinievarezeda@mail.ru>
2023-01-08 16:03:52 +0300

    проверка инпутов при событии blur

--- src/modules/validateForms.js
+++ src/modules/validateForms.js
@@ -20,9 +20,27 @@ const validate = () => {
                 if (e.target.type === 'tel') {
                     // ввод только цифр, круглых скобок и дефис
                     e.target.value = e.target.value.replace(/[^\d\(\)\-]/g, '');
-                };
+                }
             });
+
+            input.addEventListener('blur', (e) => {
+                const reg1 = /\s+/gi; //Несколько идущих подряд пробелов должны заменяться на один.
+                const reg2 = /\-+/gi; //Несколько идущих подряд дефисов должны заменяться на один.
+                const reg3 = /[-\s]+$/g;   //Пробелы и дефисы в конце значения должны удаляться.
+                const reg4 = /^[-\s]+/g;   //Пробелы и дефисы в начале значения должны удаляться.
+                const reg5 = /^[^A-ZА-ЯЁ]+[^a-zа-яё]*$/g; //Для поля type=text Первая буква каждого слова должна приводиться к верхнему регистру, а все остальные — к нижнему.
+                e.target.value = e.target.value.replace(reg1, ' ');
+                e.target.value = e.target.value.replace(reg2, '-');
+                e.target.value = e.target.value.replace(reg3, '');
+                e.target.value = e.target.value.replace(reg4, '');
+
+                if (input.type === 'text') {
+                    let newValue = e.target.value.substr(0, 1).toUpperCase() + e.target.value.substr(1, e.target.value.length - 1).toLowerCase();
+                    e.target.value = e.target.value.replace(reg5, newValue);
+                }
+            });
+
         });
-    });
+    })
 }
 export default validate 
